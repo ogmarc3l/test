df05b777fb6faa35d720d20c6dc3fcacc777ed47
ogmarc3l <142096019+ogmarc3l@users.noreply.github.com>
2023-08-12 20:17:41 +0200

    Update chuj5.js

--- chuj5.js
+++ chuj5.js
@@ -1,7 +1,1 @@
-$(window).on('load', function() {
-    $('#status').delay(2900).fadeOut();
-    $('#preloader').delay(2900).fadeOut('slow');
-    $('body').delay(2900).css({
-        'overflow': 'visible'
-    });
-});
+nie ma
